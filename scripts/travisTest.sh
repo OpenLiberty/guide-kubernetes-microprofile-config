9d16b0910a136db64d342a1a2f9a2e79a3b63afc
Eveline Cai <33134427+evelinec@users.noreply.github.com>
2019-04-04 15:14:59 -0400

    Added set error flags to report intermediate cmd failure

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -1,37 +1,29 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 ##############################################################################
 ##
 ##  Travis CI test script
 ##
 ##############################################################################
 
-printf "\nmvn -q package\n"
 mvn -q package
 
-printf "\nkubectl create configmap greeting-config --from-literal message=Greetings...\n"
 kubectl create configmap greeting-config --from-literal message=Greetings...
 
-printf "\nkubectl create secret generic name-credentials --from-literal username=bob --from-literal password=bobpwd\n"
 kubectl create secret generic name-credentials --from-literal username=bob --from-literal password=bobpwd
 
-printf "\nkubectl apply -f kubernetes.yaml\n"
 kubectl apply -f kubernetes.yaml
 
-printf "\nsleep 120\n"
 sleep 120
 
-printf "\nkubectl get pods\n"
 kubectl get pods
 
-printf "\minikube ip\n"
 echo `minikube ip`
 
-printf "\nmvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip` -Dname.message=Greetings...\n"
 mvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip` -Dname.message=Greetings...
 
-printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)\n"
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)
 
-printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)\n"
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)
