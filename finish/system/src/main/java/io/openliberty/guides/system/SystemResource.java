f0681b31a5f3384837f688fcc8b0dde5d2b237c5
Panagiotis Roubatsis <panagiotis.roubatsis@ibm.com>
2019-04-11 14:31:37 -0400

    Replace name/ping with inventory/system in finish directory

---
+++ finish/system/src/main/java/io/openliberty/guides/system/SystemResource.java
@@ -0,0 +1,49 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2017, 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+package io.openliberty.guides.system;
+
+// CDI
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+// JAX-RS
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@RequestScoped
+@Path("/properties")
+public class SystemResource {
+
+  // tag::appName[]
+  @Inject
+  @ConfigProperty(name = "APP_NAME")
+  private String appName;
+  // end::appName[]
+
+  @Inject
+  @ConfigProperty(name = "HOSTNAME")
+  private String hostname;
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getProperties() {
+    return Response.ok(System.getProperties())
+      .header("X-Pod-Name", hostname)
+      .header("X-App-Name", appName)
+      .build();
+  } 
+}
