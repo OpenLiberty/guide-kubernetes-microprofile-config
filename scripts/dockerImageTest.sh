96f155981714e51f324658f2ca3af474cde6f3ad
Austin Seto <austin.seto@ibm.com>
2020-12-16 16:56:29 -0500

    Update for testing OL docker image

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -3,18 +3,18 @@ while getopts t:d:b:u: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
-    b) BUILD="${OPTARG}" ;;
-    u) DOCKER_USERNAME="${OPTARG}" ;;
     esac
 done
 
-echo "Testing daily build image"
+echo "Testing latest OpenLiberty Docker image"
 
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" inventory/pom.xml system/pom.xml
 cat inventory/pom.xml system/pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" inventory/Dockerfile system/Dockerfile
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" inventory/Dockerfile system/Dockerfile
 cat inventory/Dockerfile system/Dockerfile
+
+docker pull "openliberty/daily:latest"
 
 sudo ../scripts/startMinikube.sh
 sudo ../scripts/testApp.sh
--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -7,19 +7,6 @@ set -euxo pipefail
 ##
 ##############################################################################
 
-# Set up Minikube
-
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo ln -s -f $(pwd)/kubectl /usr/local/bin/kubectl
-wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
-chmod +x minikube
-
-sudo apt-get update -y
-sudo apt-get install -y conntrack
-
-sudo minikube start --vm-driver=none --bootstrapper=kubeadm
-
 # Test app
 
 mvn -q package
@@ -30,8 +17,7 @@ docker build -t system:1.0-SNAPSHOT system/.
 docker build -t inventory:1.0-SNAPSHOT inventory/.
 
 kubectl create configmap sys-app-name --from-literal name=my-system -o yaml --dry-run | kubectl apply -f -
-kubectl create secret generic sys-app-credentials --from-literal username=bob --from-literal password=bobpwd --dry-run -o yaml |
-    kubectl apply -f -
+kubectl create secret generic sys-app-credentials --from-literal username=bob --from-literal password=bobpwd --dry-run -o yaml | kubectl apply -f -
 
 kubectl apply -f kubernetes.yaml
 
