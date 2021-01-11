e49f5a88c3909fdfd1a80f1cbf09558522dd838b
Gilbert Kwan <gkwan@ca.ibm.com>
2021-01-11 11:53:50 -0500

    Update stopMinikube.sh

--- scripts/stopMinikube.sh
+++ scripts/stopMinikube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl delete -f kubernetes.yaml
-kubectl delete configmap sys-app-name
+kubectl delete configmap sys-app-root
 kubectl delete secret sys-app-credentials
 
 eval $(minikube docker-env -u)
