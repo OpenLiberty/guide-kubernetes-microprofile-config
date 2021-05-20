dfcc1e1bbb240b28f49ff1015e492ac092f96067
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-20 16:35:51 -0400

    Version update (#132)
    * update versions
    
    * Update Maven options
    
    * Update dailyBuild.sh and dockerImageTest.sh
    
    Co-authored-by: Austin Seto <austinseto@gmail.com>
    Co-authored-by: Priyanshi Patel <priyanshi.patel@ibm.com>

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-while getopts t:d:b:u: flag; do
+while getopts t:d: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
+    *) echo "Invalid option";;
     esac
 done
 
 echo "Testing latest OpenLiberty Docker image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" inventory/pom.xml system/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/""$DATE""/""$DRIVER""</runtimeUrl></install></configuration>" inventory/pom.xml system/pom.xml
 cat inventory/pom.xml system/pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" inventory/Dockerfile system/Dockerfile
+sed -i "s;FROM openliberty/open-liberty:full-java11-openj9-ubi;FROM openliberty/daily:latest;g" inventory/Dockerfile system/Dockerfile
 cat inventory/Dockerfile system/Dockerfile
 
 docker pull "openliberty/daily:latest"
