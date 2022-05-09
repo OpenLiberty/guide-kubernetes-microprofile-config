ca08a102b59ff4be42d93c40cd2fcab99b1ed013
t27gupta <tushar.gupta@uwaterloo.ca>
2022-05-09 17:46:17 -0400

    linter fix

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryStartupCheck.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryStartupCheck.java
@@ -1,14 +1,14 @@
 // tag::copyright[]
 /*******************************************************************************
-* Copyright (c) 2022 IBM Corporation and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*     IBM Corporation - Initial implementation
-*******************************************************************************/
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
 // end::copyright[]
 // tag::InventoryStartupCheck[]
  package io.openliberty.guides.inventory.health;
