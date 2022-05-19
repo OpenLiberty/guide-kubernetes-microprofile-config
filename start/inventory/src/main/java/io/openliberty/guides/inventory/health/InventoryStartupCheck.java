a6b7cc9aa4faef379e0531ceef2d0a6a2086ceb8
Gilbert Kwan <gkwan@ca.ibm.com>
2022-05-19 10:51:43 -0400

    Update InventoryStartupCheck.java

--- start/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryStartupCheck.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryStartupCheck.java
@@ -22,12 +22,9 @@ import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Startup;
 
-
 import jakarta.enterprise.context.ApplicationScoped;
 
-
 @Startup
-
 @ApplicationScoped
 public class InventoryStartupCheck implements HealthCheck {
 
