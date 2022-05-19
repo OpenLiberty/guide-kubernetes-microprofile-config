c91d7d3fbb8e02b336e10d08e68b8d6be732af74
Gilbert Kwan <gkwan@ca.ibm.com>
2022-05-19 10:42:41 -0400

    Update InventoryStartupCheck.java

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryStartupCheck.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryStartupCheck.java
@@ -11,35 +11,31 @@
  *******************************************************************************/
 // end::copyright[]
 // tag::InventoryStartupCheck[]
- package io.openliberty.guides.inventory.health;
+package io.openliberty.guides.inventory.health;
 
- import java.lang.management.ManagementFactory;
- import io.openliberty.guides.inventory.InventoryResource;
+import java.lang.management.ManagementFactory;
+import io.openliberty.guides.inventory.InventoryResource;
 
- import com.sun.management.OperatingSystemMXBean;
+import com.sun.management.OperatingSystemMXBean;
 
- import org.eclipse.microprofile.health.HealthCheck;
- import org.eclipse.microprofile.health.HealthCheckResponse;
- import org.eclipse.microprofile.health.Startup;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Startup;
 
+import jakarta.enterprise.context.ApplicationScoped;
 
- import jakarta.enterprise.context.ApplicationScoped;
+@Startup
+@ApplicationScoped
+public class InventoryStartupCheck implements HealthCheck {
 
-
- @Startup
-
- @ApplicationScoped
- public class InventoryStartupCheck implements HealthCheck {
-
-     @Override
-     public HealthCheckResponse call() {
-         OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean)
-         ManagementFactory.getOperatingSystemMXBean();
-         double cpuUsed = bean.getSystemCpuLoad();
-         String cpuUsage = String.valueOf(cpuUsed);
-         return HealthCheckResponse.named(InventoryResource.class
+    @Override
+    public HealthCheckResponse call() {
+        OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean)
+        ManagementFactory.getOperatingSystemMXBean();
+        double cpuUsed = bean.getSystemCpuLoad();
+        String cpuUsage = String.valueOf(cpuUsed);
+        return HealthCheckResponse.named(InventoryResource.class
                                              .getSimpleName() + " Startup Check")
                                              .status(cpuUsed < 0.95).build();
-     }
- }
-
+    }
+}
