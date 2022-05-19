77bdee26cd021061134f48776af20e0506effea7
Gilbert Kwan <gkwan@ca.ibm.com>
2022-05-19 10:47:34 -0400

    Update SystemReadinessCheck.java

--- finish/system/src/main/java/io/openliberty/guides/system/health/SystemReadinessCheck.java
+++ finish/system/src/main/java/io/openliberty/guides/system/health/SystemReadinessCheck.java
@@ -10,39 +10,39 @@
  *     IBM Corporation - Initial implementation
  *******************************************************************************/
 // end::copyright[]
- package io.openliberty.guides.system.health;
+package io.openliberty.guides.system.health;
 
- import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
- import io.openliberty.guides.system.SystemResource;
- import jakarta.enterprise.context.ApplicationScoped;
- import org.eclipse.microprofile.health.Readiness;
- import org.eclipse.microprofile.health.HealthCheck;
- import org.eclipse.microprofile.health.HealthCheckResponse;
+import io.openliberty.guides.system.SystemResource;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
 
- @Readiness
- @ApplicationScoped
- public class SystemReadinessCheck implements HealthCheck {
+@Readiness
+@ApplicationScoped
+public class SystemReadinessCheck implements HealthCheck {
 
-     private static final int ALIVE_DELAY_SECONDS = 60;
-     private static final String READINESS_CHECK = SystemResource.class.getSimpleName()
+    private static final int ALIVE_DELAY_SECONDS = 60;
+    private static final String READINESS_CHECK = SystemResource.class.getSimpleName()
                                                   + " Readiness Check";
-     private static LocalDateTime aliveAfter = LocalDateTime.now();
+    private static LocalDateTime aliveAfter = LocalDateTime.now();
 
-     @Override
-     public HealthCheckResponse call() {
-         if (isAlive()) {
-             return HealthCheckResponse.up(READINESS_CHECK);
-         }
+    @Override
+    public HealthCheckResponse call() {
+        if (isAlive()) {
+            return HealthCheckResponse.up(READINESS_CHECK);
+        }
 
-         return HealthCheckResponse.down(READINESS_CHECK);
-     }
+        return HealthCheckResponse.down(READINESS_CHECK);
+    }
 
-     public static void setUnhealthy() {
-         aliveAfter = LocalDateTime.now().plusSeconds(ALIVE_DELAY_SECONDS);
-     }
+    public static void setUnhealthy() {
+        aliveAfter = LocalDateTime.now().plusSeconds(ALIVE_DELAY_SECONDS);
+    }
 
-     private static boolean isAlive() {
-         return LocalDateTime.now().isAfter(aliveAfter);
-     }
- }
+    private static boolean isAlive() {
+        return LocalDateTime.now().isAfter(aliveAfter);
+    }
+}
