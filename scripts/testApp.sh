7df5e02b3da7400beef4cd229da28806c4e0b3e7
Gilbert Kwan <gkwan@ca.ibm.com>
2022-07-05 16:37:23 -0400

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -7,10 +7,11 @@ set -euxo pipefail
 ##
 ##############################################################################
 
-# ../scripts/startMinikube.sh
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+../scripts/startMinikube.sh
+
+#SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # shellcheck source=./scripts/startMinikube.sh
-source "$SCRIPTPATH"/startMinikube.sh
+#source "$SCRIPTPATH"/startMinikube.sh
 
 # Test app
 
@@ -47,6 +48,6 @@ mvn failsafe:verify
 kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)"
 kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)"
 
-# ../scripts/stopMinikube.sh
+../scripts/stopMinikube.sh
 # shellcheck source=./scripts/stopMinikube.sh
-source "$SCRIPTPATH"/stopMinikube.sh
+#source "$SCRIPTPATH"/stopMinikube.sh
