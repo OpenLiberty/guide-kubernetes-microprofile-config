f0e544460736c0678b35741e288d5d27da6055ec
Gilbert Kwan <gkwan@ca.ibm.com>
2023-07-10 15:54:26 -0400

    Skip features.sh in docker test scripts

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -13,6 +13,7 @@ sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install
 cat inventory/pom.xml system/pom.xml
 
 sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM openliberty/daily:latest;g" inventory/Dockerfile system/Dockerfile
+sed -i "s;RUN features.sh;#RUN features.sh;g" inventory/Dockerfile system/Dockerfile
 cat inventory/Dockerfile system/Dockerfile
 
 docker pull -q "openliberty/daily:latest"
