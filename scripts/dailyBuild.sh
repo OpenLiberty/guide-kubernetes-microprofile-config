f5aa98dff2f546a4d1b2f22761ee24410b6f914a
Gilbert Kwan <gkwan@ca.ibm.com>
2024-04-23 14:26:43 -0400

    Update dailyBuild

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -3,7 +3,6 @@ while getopts t:d:b:u: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
-    b) BUILD="${OPTARG}" ;;
     *) echo "Invalid option";;
     esac
 done
