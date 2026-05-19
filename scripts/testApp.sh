7f43ceeab230d534a315acebcc0cefd50c83083f
Siaa G <siaa.gor@ibm.com>
2026-05-19 16:51:01 -0400

    Remove debug commands/logs from testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -42,16 +42,6 @@ kubectl get pods
 minikube ip
 curl "http://$(minikube ip):31000/health"
 curl "http://$(minikube ip):32000/health"
-
-# Debug
-kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)"
-
-kubectl exec "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)" -- env | grep SYSTEM_APP
-kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)"
-
-kubectl get secret sys-app-credentials -o jsonpath='{.data.username}' | base64 -d
-kubectl get secret sys-app-credentials -o jsonpath='{.data.password}' | base64 -d
-
 ./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
